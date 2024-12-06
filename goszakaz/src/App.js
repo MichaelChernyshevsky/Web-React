f7ff257026eef12ea979facfd6239007bfaf31fe
Mischa <michael.chernyshevsky@yandex.ru>
2024-12-06 14:41:00 +0300

    - initial

---
+++ goszakaz/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Goszakaz } from './index.jsx'; 
+
+function App() {
+  return (
+    <div>
+      hello
+      <Goszakaz/>
+    </div>
+  );
+}
+
+export default App;
---
+++ goszakaz/src/index.jsx
@@ -0,0 +1,431 @@
+import React from "react";
+
+import "./style.css";
+
+
+export const Goszakaz = () => {
+  return (
+    <div className="goszakaz">
+      <div className="div">
+        <div className="overlap">
+          <footer className="footer">
+            <div className="overlap-group">
+              <div className="overlap-group-wrapper">
+                <div className="overlap-group-2">
+                  <div className="phone-number" />
+
+                  <div className="phone-number-2" />
+
+                  <p className="element">
+                    <span className="text-wrapper">
+                      Адрес бек-офиса: г. Н.Новгород, ул. М.Горького, 117
+                      <br />
+                      Время работы: 07:00 – 20:00 по МСК
+                    </span>
+                  </p>
+
+                  <div className="group">
+                    <div className="text-wrapper-2">+7 (800) 600–21–57</div>
+
+                    {/* <img className="vector" alt="Vector" src={vector} /> */}
+                  </div>
+                </div>
+              </div>
+
+              <div className="div-2">
+                <p className="p">
+                  Ваш персональный менеджер всегда доступен и отвечает
+                  <br />
+                  на звонки, письма и сообщения.
+                </p>
+
+                <div className="text-wrapper-3">Наши контакты</div>
+              </div>
+            </div>
+          </footer>
+
+          <div className="element-2">
+            <div className="overlap-2">
+              <div className="qa">
+                <p className="div-3">
+                  <span className="span">
+                    Выпустить банковскую гарантию за 1 день реально?
+                    <br />
+                  </span>
+
+                  <span className="text-wrapper-4">
+                    Банковские гарантии на сумму до 50 млн. рублей можно
+                    оформить в день подачи заявки. Главное оперативно
+                    предоставить пакет документов и подписать заявки ЭЦП.
+                  </span>
+                </p>
+
+                {/* <img className="frame" alt="Frame" src={image1} /> */}
+              </div>
+
+              <div className="qa-2">
+                <p className="div-4">
+                  <span className="span">
+                    Что делать если банки отказывают?
+                    <br />
+                  </span>
+
+                  <span className="text-wrapper-4">
+                    Самые частые причины отказов: ошибки в документации и низкий
+                    рейтинг по скорингу банка. Мы поможем правильно оформить
+                    документы и подскажем как компенсировать слабые финансовые
+                    показатели.
+                  </span>
+                </p>
+
+                 {/* <img className="img" alt="Frame" src={frame62} /> */}
+              </div>
+            </div>
+
+            <div className="overlap-3">
+              <div className="overlap-wrapper">
+                <div className="overlap-4">
+                  <div className="overlap-group-3">
+                    <p className="div-5">
+                      <span className="span">
+                        В интернете полно мошенников, как быть уверенными, что
+                        вы не обманете?
+                        <br />
+                      </span>
+
+                      <span className="text-wrapper-4">
+                        Мы официальные представители банков и имеем
+                        подтверждающие это договоры. Комиссию за выпуск
+                        банковской гарантии вы всегда оплачиваете напрямую в
+                        банк, рисков обмана и мошенничества нет.
+                      </span>
+                    </p>
+
+                    <div className="qa-3">
+                      <p className="div-3">
+                        <span className="span">
+                          Можно ли получить гарантию с нулевым или убыточным
+                          балансом?
+                          <br />
+                        </span>
+
+                        <span className="text-wrapper-4">
+                          Да можно. Финансовые показатели не единственный
+                          критерий одобрения выдачи гарантии.
+                        </span>
+                      </p>
+
+                      {/* <img className="frame" alt="Frame" src={frame68} /> */}
+                    </div>
+                  </div>
+
+                  {/* <img className="frame" alt="Frame" src={frame67} /> */}
+                </div>
+              </div>
+
+              <div className="qa-4">
+                <p className="div-3">
+                  <span className="span">
+                    Мы недавно зарегистрировали ООО / ИП, нам дадут гарантию?
+                    <br />
+                  </span>
+
+                  <span className="text-wrapper-4">
+                    Большинство банков отказывают компаниям моложе 6 месяцев. Мы
+                    знаем банки лояльные к молодым компаниям. Имеем опыт выпуска
+                    гарантий для компаний которым 1 неделя с момента
+                    регистрации.
+                  </span>
+                </p>
+
+                {/* <img className="frame" alt="Frame" src={frame65} /> */}
+              </div>
+
+              <div className="qa-5">
+                <p className="div-6">
+                  <span className="span">
+                    Работаете с Крымом / Кавказом?
+                    <br />
+                  </span>
+
+                  <span className="text-wrapper-4">
+                    Для многих банков Крым и Северный Кавказ — стоп-фактор. Мы
+                    подберем банки, которые работают даже с этими регионами.
+                  </span>
+                </p>
+
+                {/* <img className="frame-2" alt="Frame" src={frame63} /> */}
+              </div>
+
+              <div className="qa-6">
+                <p className="div-7">
+                  <span className="span">
+                    Это наш первый тендер, нам выдадут гарантию?
+                    <br />
+                  </span>
+
+                  <span className="text-wrapper-4">
+                    Отсутствие опыта исполнения госконтрактов не помеха —
+                    выпустим гарантию без обременения в виде открытия р/с,
+                    залогов и поручительства.
+                  </span>
+                </p>
+
+                {/* <img className="frame-2" alt="Frame" src={frame64} /> */}
+              </div>
+
+              <div className="qa-7">
+                <p className="div-3">
+                  <span className="span">
+                    У нас нет всех требуемых документов, что делать?
+                    <br />
+                  </span>
+
+                  <span className="text-wrapper-4">
+                    Мы знаем как выпуститься с минимальным пакетом документов. В
+                    некоторых случаях достаточно 3-х документов.
+                  </span>
+                </p>
+
+                {/* <img className="frame" alt="Frame" src={frame66} /> */}
+              </div>
+            </div>
+
+            <div className="text-wrapper-5">Часто задаваемые вопросы</div>
+          </div>
+        </div>
+
+        <div className="element-3">
+          <button className="button">
+            <div className="div-wrapper">
+              <div className="text-wrapper-6">Получить анализ</div>
+            </div>
+          </button>
+
+          <div className="phone-number-3">
+            <div className="overlap-5">
+              <div className="text-wrapper-7">+7 (999) 999–99–99</div>
+
+              <div className="rectangle" />
+            </div>
+          </div>
+
+          <div className="link">
+            <div className="overlap-5">
+              <div className="text-wrapper-7">Ссылка на тендер</div>
+
+              <div className="rectangle" />
+            </div>
+          </div>
+
+          <div className="overlap-6">
+            <p className="text-wrapper-8">
+              и получите полный список документов именно для вашей банковской
+              гарантии
+            </p>
+
+            <div className="text-wrapper-9">Отправьте ссылку на тендер</div>
+
+            <div className="overlap-7">
+              {/* <img className="bg" alt="Bg" src={bg} /> */}
+
+              <div className="logos">
+                {/* <img className="element-4" alt="Element" src={x22} /> */}
+
+                {/* <img className="element-5" alt="Element" src={x21} /> */}
+
+                {/* <img className="photo" alt="Photo" src={photo1} /> */}
+
+                {/* <img className="element-6" alt="Element" src={x31} /> */}
+
+                {/* <img className="fz" alt="Fz" src={fz4441} /> */}
+
+                {/* <img className="xbaqjo" alt="Xbaqjo" src={x8baqjo1} /> */}
+
+                {/* <img className="element-7" alt="Element" src={x12} /> */}
+
+                {/* <img className="element-8" alt="Element" src={x41} /> */}
+              </div>
+            </div>
+          </div>
+
+          <p className="div-8">
+            <span className="text-wrapper-10">
+              Работаем с тендерными площадками{" "}
+            </span>
+
+            <span className="text-wrapper-11">по всей России</span>
+          </p>
+        </div>
+
+        <div className="element-9">
+          <div className="logos-2">
+            <div className="overlap-group-4">
+              <div className="group-2">
+                {/* <img
+                  className="element-sberbank-logo"
+                  alt="Element sberbank logo"
+                  src={pxSberbankLogo1}
+                />
+
+                <img className="uralsib" alt="Uralsib" src={uralsib1} />
+
+                <img className="unnamed" alt="Unnamed" src={unnamed1} />
+
+                <img className="logo-gpb" alt="Logo gpb" src={logoGpb01} />
+
+                <img className="element-10" alt="Element" src={image} /> */}
+              </div>
+
+              <div className="group-3">
+                {/* <img
+                  className="absolut-bank"
+                  alt="Absolut bank"
+                  src={absolutBank1}
+                />
+
+                <img className="alfa-bank" alt="Alfa bank" src={alfaBank1} />
+
+                <img
+                  className="element-VTB-logo"
+                  alt="Element VTB logo"
+                  src={pxVtbLogo20181}
+                /> */}
+              </div>
+            </div>
+          </div>
+
+          <p className="element-11">
+            <span className="text-wrapper-10">Вы получаете </span>
+
+            <span className="text-wrapper-11">
+              наиболее выгодные предложения
+            </span>
+
+            <span className="text-wrapper-10">
+              {" "}
+              от банков-партнеров в течение 1 часа
+            </span>
+          </p>
+        </div>
+
+        <div className="element-12">
+          <div className="div-9">
+            <div className="group-4">
+              <div className="text-wrapper-12">Прямые договоры</div>
+
+              <div className="c">C 80 БАНКАМИ</div>
+            </div>
+
+            <p className="text-wrapper-13">
+              Минимальные ставки <br />и дополнительные скидки
+              <br />
+              по комиссии.
+            </p>
+
+            {/* <img className="img-2" alt="Bank" src={bank1} /> */}
+          </div>
+
+          <div className="div-10">
+            <p className="div-11">
+              <span className="text-wrapper-10">Работаем с </span>
+
+              <span className="text-wrapper-11">НОВЫМИ</span>
+
+              <span className="text-wrapper-10"> ООО и ИП</span>
+            </p>
+
+            <p className="element-13">
+              Помогаем оформить банковскую гарантию даже компаниям
+              <br />с возрастом от 1 дня.
+            </p>
+
+            {/* <img className="team" alt="Team" src={team1} /> */}
+          </div>
+
+          <div className="div-12">
+            <div className="group-wrapper">
+              <div className="group-5">
+                <div className="overlap-group-5">
+                  <div className="text-wrapper-14">
+                    Оказываем
+                    <br />
+                    сопровождение
+                  </div>
+
+                  <div className="text-wrapper-15">ПОЛНОЕ</div>
+                </div>
+              </div>
+            </div>
+
+            <p className="text-wrapper-16">
+              Проводим по всем этапам, а если потребуется даже удаленно
+              подключимся к Вашему компьютеру
+              <br />и сделаем всё за вас.
+            </p>
+
+            {/* <img className="img-2" alt="Support" src={support1} /> */}
+          </div>
+        </div>
+
+        <div className="element-14">
+          <div className="hero">
+            <div className="phone-number-4">
+              <div className="overlap-group-6">
+                <div className="text-wrapper-17">+7 (999) 999–99–99</div>
+
+                <div className="rectangle-2" />
+              </div>
+            </div>
+
+            <button className="button-2">
+              <div className="overlap-8">
+                <div className="text-wrapper-18">Получить гарантию</div>
+
+                <div className="rectangle-3" />
+              </div>
+            </button>
+
+            <div className="text-wrapper-19">
+              Гарантия с бесплатным оформлением!
+            </div>
+
+            <div className="pointer">
+              <p className="text-wrapper-20">
+                Персональный список документов для контракта
+              </p>
+
+              {/* <img className="frame-3" alt="Frame" src={frame5} /> */}
+            </div>
+
+            <div className="pointer-2">
+              <div className="text-wrapper-20">
+                Предварительный расчет процентной ставки
+              </div>
+
+              {/* <img className="frame-3" alt="Frame" src={frame6} /> */}
+            </div>
+
+            <div className="rectangle-wrapper">
+              <div className="rectangle-4" />
+            </div>
+          </div>
+
+          <div className="div-13">
+            <div className="group-6">
+              <div className="group-7">
+                <div className="text-wrapper-21">
+                  Гарантийный фонд Малого бизнеса
+                </div>
+              </div>
+            </div>
+
+            <p className="text-wrapper-22">
+              Находим ключ к решению всех вопросов
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
